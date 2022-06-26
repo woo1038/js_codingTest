fd3019fe16e99af4b1d6d744c0740359caa71a82
hyukim <woo1038>
2022-06-26 14:39:08 +0900

    update section 8

--- section9/sec08.js
+++ section9/sec08.js
@@ -1,19 +1,23 @@
 function solution(arr) {
   let result = 0;
+  let queue = [];
 
+  const len = arr.length;
   const dx = [-1, -1, 0, 1, 1, 1, 0, -1];
   const dy = [0, 1, 1, 1, 0, -1, -1, -1];
-  const len = arr.length;
 
-  function DFS(x, y) {
-    arr[x][y] = 0;
+  function BFS() {
+    while (queue.length) {
+      let [x, y] = queue.shift();
 
-    for (let j = 0; j < 8; j++) {
-      let nx = x + dx[j];
-      let ny = y + dy[j];
+      for (let j = 0; j < len; j++) {
+        let nx = x + dx[j];
+        let ny = y + dy[j];
 
-      if (nx >= 0 && nx < len && ny >= 0 && ny < len && arr[nx][ny] === 1) {
-        DFS(nx, ny);
+        if (nx >= 0 && nx < len && ny >= 0 && ny < len && arr[nx][ny] === 1) {
+          arr[nx][ny] = 0;
+          queue.push([nx, ny]);
+        }
       }
     }
   }
@@ -21,8 +25,10 @@ function solution(arr) {
   for (let i = 0; i < len; i++) {
     for (let k = 0; k < len; k++) {
       if (arr[i][k] === 1) {
+        arr[i][k] = 0;
+        queue.push([i, k]);
         result++;
-        DFS(i, k);
+        BFS();
       }
     }
   }
